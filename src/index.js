be95050f1d396f8e9134394a9eafc0b44bbb4b56
Katarzyna Maculewicz <qla.katarzyna@gmail.com>
2025-12-04 00:04:11 +0000

    Application file structure implemented, App component updated

--- src/index.js
+++ src/index.js
@@ -1,19 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'normalize.css';
-import { ThemeProvider } from "styled-components";
-import { theme } from "./theme";
-import App from './App';
-import { GlobalStyle } from './GlobalStyle';
+import { App } from './core/App/index';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
       <App />
-    </ThemeProvider>
   </React.StrictMode>
 );
 
