bc54c32ea38bf5b33036212038c3720c8e482028
Joanna Czub <jk.czub@student.uw.edu.pl>
2025-12-15 19:52:19 +0100

    MoviePage added

--- src/features/Movies/MoviePage/index.js
+++ src/features/Movies/MoviePage/index.js
@@ -2,17 +2,28 @@ import { Wrapper, MovieDetails } from "./styled";
 import MovieSection from "./Container";
 import MovieTileSection from "./MovieTile";
 import Cast from "./PeopleSection";
+import { useParams } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { selectMovieList } from "../movieSlice";
 
 export const MoviePage = () => {
-    return (
-        <Wrapper>
+  const { id } = useParams();
+  const movies = useSelector(selectMovieList);
+  const movie = movies.find((m) => m.id.toString() === id);
+
+  if (!movie) return <p>Film nie znaleziony</p>;
+
+  return (
+    <div style={{ padding: "20px" }}>
+      <Wrapper>
             <MovieSection />
             <MovieDetails>
                 <MovieTileSection />
                 <Cast />
             </MovieDetails>
         </Wrapper>
-    );
+    </div>
+  );
 };
 
 export default MoviePage;
