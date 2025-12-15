f4783175ea4f4faeaa2357739f1d341d2c1b2861
Katarzyna Maculewicz <qla.katarzyna@gmail.com>
2025-12-15 18:34:13 +0000

    feat: change search input placeholder based on active route

--- src/common/Navigation/SearchBar/index.js
+++ src/common/Navigation/SearchBar/index.js
@@ -1,11 +1,20 @@
+import { useLocation } from "react-router-dom";
 import { RightWrapper, SearchWrapper, Input, SearchIcon } from "./styled";
 
 export const SearchBar = () => {
+  const location = useLocation();
+
+  const isMovies = location.pathname.startsWith("/movies");
+
+  const placeholder = isMovies
+    ? "Search for movies..."
+    : "Search for people...";
+
   return (
     <RightWrapper>
       <SearchWrapper>
         <SearchIcon />
-        <Input placeholder="Search for people…" />
+        <Input placeholder={placeholder} />
       </SearchWrapper>
     </RightWrapper>
   );
