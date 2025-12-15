07cb83ca77fd1a6b430b36c2da5cd1082ac858ea
Katarzyna Maculewicz <qla.katarzyna@gmail.com>
2025-12-15 21:31:44 +0000

    feat: implement onInputChange handler

--- src/common/Navigation/SearchBar/index.js
+++ src/common/Navigation/SearchBar/index.js
@@ -1,8 +1,9 @@
-import { useLocation } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import { RightWrapper, SearchWrapper, Input, SearchIcon } from "./styled";
 
 export const SearchBar = () => {
   const location = useLocation();
+  const navigate = useNavigate();
 
   const isMovies = location.pathname.startsWith("/movies");
 
@@ -10,11 +11,20 @@ export const SearchBar = () => {
     ? "Search for movies..."
     : "Search for people...";
 
+  const onInputChange = ({ target }) => {
+    const query = target.value;
+
+    navigate({
+      pathname: isMovies ? "/movies" : "/people",
+      search: query ? `?query=${query}` : "",
+    });
+  };
+
   return (
     <RightWrapper>
       <SearchWrapper>
         <SearchIcon />
-        <Input placeholder={placeholder} />
+        <Input placeholder={placeholder} onChange={onInputChange} />
       </SearchWrapper>
     </RightWrapper>
   );
