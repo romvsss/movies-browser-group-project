b87ed9121a4040861ccf9645b36a46bb624679ec
Katarzyna Maculewicz <qla.katarzyna@gmail.com>
2025-12-16 23:55:22 +0000

    feat: add PeopleList component with actors list

--- src/features/Peoples/PeopleList/index.js
+++ src/features/Peoples/PeopleList/index.js
@@ -1,3 +1,28 @@
+import { useDispatch, useSelector } from "react-redux";
+import { useLocation } from "react-router-dom";
+
 export const PeopleList = () => {
-  return <h1>COMING SOON</h1>
+  const dispatch = useDispatch();
+  const location = useLocation();
+
+  const people = useSelector((state) => state.people.peopleList);
+  const status = useSelector((state) => state.people.status);
+
+  if (status === "loading") {
+    return <p>Loading...</p>;
+  }
+
+  if (!people || people.length === 0) {
+    return <p>Brak wyników</p>;
+  }
+
+  return (
+    <div style={{ display: "grid", gap: 16 }}>
+      {people.map((person) => (
+        <div key={person.id}>
+          <h3>{person.name}</h3>
+        </div>
+      ))}
+    </div>
+  );
 };
