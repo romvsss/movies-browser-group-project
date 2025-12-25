718b2f74b50fc84b16e63c86861bd7b246799085
Joanna Czub <jk.czub@student.uw.edu.pl>
2025-12-25 14:59:07 +0100

    Loading and Error added

--- src/features/Movies/MoviePage/index.js
+++ src/features/Movies/MoviePage/index.js
@@ -2,6 +2,8 @@ import { Wrapper, MovieDetails } from "./styled";
 import MovieSection from "./Container";
 import MovieTileSection from "./MovieTile";
 import Cast from "./PeopleSection";
+import Loading from '../../../common/Navigation/SearchBar/Content/Loading';
+import Error from "../../../common/Navigation/SearchBar/Content/Error"
 import { useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
@@ -23,8 +25,8 @@ export const MoviePage = () => {
     dispatch(fetchMovieDetails(id));
   }, [dispatch, id]);
 
-  if (status === "loading") return <div>Loading...</div>;
-  if (status === "error") return <div>Błąd ładowania</div>;
+  if (status === "loading") return <Loading />;
+  if (status === "error") return <Error />;
   if (!movie) return null;
 
   return (
